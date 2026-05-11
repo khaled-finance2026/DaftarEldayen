746bab007d82d7f74a33bf9ec38d22846c43be6d
khaled-finance2026 <khfinance2026@gmail.com>
2026-05-11 09:36:48 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,60 +1,34 @@
-const CACHE = 'daftardain-v2';
+const CACHE = 'dd-v3';
 const SCOPE = '/DaftarEldayen/';
-
-const PRECACHE = [
-  SCOPE,
-  SCOPE + 'index.html',
-  SCOPE + 'manifest.json',
-  'https://unpkg.com/dexie@3/dist/dexie.js'
-];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE)
-      .then(c => c.addAll(PRECACHE).catch(() => {}))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE).then(c => c.addAll([
+      SCOPE,
+      SCOPE + 'index.html',
+      SCOPE + 'manifest.json',
+      'https://unpkg.com/dexie@3/dist/dexie.js'
+    ]).catch(() => {})).then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
-        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-      ))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
   if (e.request.method !== 'GET') return;
-
+  const url = new URL(e.request.url);
   if (url.hostname === 'khaled-finance2026.github.io') {
     e.respondWith(
-      caches.match(e.request)
-        .then(cached => cached || fetch(e.request)
-          .then(res => {
-            if (res.ok) {
-              caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-            }
-            return res;
-          })
-        )
-    );
-    return;
-  }
-
-  if (url.hostname.includes('supabase.co')) {
-    e.respondWith(
-      fetch(e.request)
-        .then(res => {
-          if (res.ok) {
-            caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-          }
-          return res;
-        })
-        .catch(() => caches.match(e.request))
+      caches.match(e.request).then(c => c || fetch(e.request).then(r => {
+        if (r.ok) caches.open(CACHE).then(cache => cache.put(e.request, r.clone()));
+        return r;
+      }))
     );
   }
 });
